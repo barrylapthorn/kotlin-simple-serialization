e862e5f336df1dd7ffec07cbdf88a8a032fa0bdd
Barry Lapthorn <barry@lapthorn.net>
2021-05-25 17:18:36 +0100

    Trivial serialize and deserialize.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple Kotlin Serialization
+
+Just shows `readObject` and `writeObject`.
+
+I wanted to find out how it worked so I could inspect the callstack.
--- app/src/main/kotlin/net/lapthorn/demo/serialization/app/App.kt
+++ app/src/main/kotlin/net/lapthorn/demo/serialization/app/App.kt
@@ -4,8 +4,29 @@
 package net.lapthorn.demo.serialization.app
 
 import net.lapthorn.demo.serialization.cars.Car
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+
 
 fun main() {
     val car = Car("Ford")
-    println("Car is ${car.name}")
+    println("Serialized Car is a ${car.name}")
+
+    val byteArrayOutputStream = ByteArrayOutputStream()
+    ObjectOutputStream(byteArrayOutputStream).use { oos ->
+        oos.writeObject(car)
+        oos.flush()
+    }
+    byteArrayOutputStream.flush()
+    val bytes = byteArrayOutputStream.toByteArray()
+    byteArrayOutputStream.close()
+
+    val byteArrayInputStream = ByteArrayInputStream(bytes)
+    ObjectInputStream(byteArrayInputStream).use {
+        val newCar = it.readObject() as Car
+        println("Deserialized Car is a ${newCar.name}")
+    }
+    byteArrayInputStream.close()
 }
